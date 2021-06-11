4841b113548838cd31a5efff39539facfd5b7a20
Tiago Boer Casagrande <tiago.boer.casagrande@ibm.com>
2021-06-11 11:05:23 -0300

    New build system and fix cnpj mask

--- src/masks/cnpj.ts
+++ src/masks/cnpj.ts
@@ -5,22 +5,22 @@ const validate = (value: string) => {
   const empty = (value || '').trim().length === 0;
   if (empty) return false;
 
-  const valida = new Array(6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2);
+  const check = new Array(6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2);
+
   let dig1 = 0;
   let dig2 = 0;
 
-  const exp = /\.|\-|\//g;
-  const cnpj = value.toString().replace(exp, '');
-  const digito = +eval(cnpj.charAt(12) + cnpj.charAt(13));
+  const cnpj = helpers.toNumber(value.toString());
+  const dig = +((cnpj.charAt(12) === '' ? ' ' : cnpj.charAt(12)) + (cnpj.charAt(13) === '' ? ' ' : cnpj.charAt(13)));
 
-  for (let i = 0; i < valida.length; i++) {
-    dig1 += i > 0 ? +cnpj.charAt(i - 1) * valida[i] : 0;
-    dig2 += +cnpj.charAt(i) * valida[i];
+  for (let i = 0; i < check.length; i++) {
+    dig1 += i > 0 ? +cnpj.charAt(i - 1) * check[i] : 0;
+    dig2 += +cnpj.charAt(i) * check[i];
   }
   dig1 = dig1 % 11 < 2 ? 0 : 11 - (dig1 % 11);
   dig2 = dig2 % 11 < 2 ? 0 : 11 - (dig2 % 11);
 
-  return dig1 * 10 + dig2 == digito;
+  return dig1 * 10 + dig2 == dig;
 };
 
 const cnpj = {
